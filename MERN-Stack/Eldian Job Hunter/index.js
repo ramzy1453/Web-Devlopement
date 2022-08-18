b0be9dab09d69c187cda0ed389bb74b63cb19964
ramzy1453 <kemmounramzy93@gmail.com>
2022-08-18 15:20:10 +0100

    Added Eldian Job Hunter

---
+++ MERN-Stack/Eldian Job Hunter/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "express-async-errors";
+import { connectDB, connectServer } from "./config";
+import notFoundMiddleware from "./middlewares/notFound";
+import errorHandlerMiddleware from "./middlewares/errorHandler";
+import authRouter from "./routes/auth";
+import jobRouter from "./routes/job";
+import authMiddleware from "./middlewares/authMiddleware";
+
+// Server
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to Eldian Job Hunter API" });
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/job", [authMiddleware], jobRouter);
+
+app.use(notFoundMiddleware());
+app.use(errorHandlerMiddleware());
+
+// Connection
+connectDB();
+connectServer(app);
